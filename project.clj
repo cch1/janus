85867947ede240aa101a69ba63b68ac1810a6d8e
Chris Hapgood <cch1@hapgoods.com>
2017-02-08 22:22:43 -0500

    Update leon-release tasks

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[com.roomkey/lein-v "6.1.0"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"]
+                  ["vcs" "push"]
+                  ["deploy"]]
   :min-lein-version "2.5.2"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
